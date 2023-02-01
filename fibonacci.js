3911d6a35c2ea6d738950a5fbb21416abf590e78
NadyaCodes <nadyacorscadden@gmail.com>
2023-02-01 16:31:54 -0500

    make first file

---
+++ fibonacci.js
@@ -0,0 +1,5 @@
+//Given a number 'n', find the first 'n' elements of the Fibonacci sequence
+//Fibonacci sequence is a sequence where each number is the sum of the two preceding ones, starting with 0 and 1
+//fibonacci(2) = [0, 1]
+//fibonacci(3) = [0, 1, 1]
+//fibonacci(7) = [0, 1, 1, 2, 3, 5, 8]
